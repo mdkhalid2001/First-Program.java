d8b9e5307c736b640532553058859c18f0431e2f
Md Hasibul Khalid Shuvo <91559654+mdkhalid2001@users.noreply.github.com>
2021-12-03 09:42:53 -0800

    first commit

---
+++ src/main/java/cse/beganner/MyApp.java
@@ -0,0 +1,15 @@
+
+package cse.beganner;
+
+
+public class MyApp {
+    public static void main(String[] args) {
+      
+        System.out.println("MD Hasibul Khalid");
+        System.out.println("01629474941");
+        System.out.println("Comes From Mymensingh");
+    }
+    
+}
+
+
